4c20ae56b9baac1de9e561358607d9c937fa621a
Dreamazium <66011917+Dreamazium@users.noreply.github.com>
2020-08-07 18:15:27 +0100

    Create test.js

---
+++ test.js
@@ -0,0 +1,64 @@
+/*eslint-env browser*/
+
+/***********
+ * COVID19 Statistics App
+ * 
+ * File modified by: (Tony McDonald)
+ ************/
+
+/**
+ * This is the main class
+ */
+var app = {
+	initialize: function () {
+        
+        // this.onDeviceReady();
+		document.addEventListener(
+			"deviceready",
+			this.onDeviceReady(this),
+			false
+        );
+	},
+	// deviceready Event Handler
+	onDeviceReady: function () {
+        //alert("hello");
+		this.receivedEvent("deviceready");
+	},
+	// Update DOM on a Received Event
+	receivedEvent: function (id) {
+       
+        function myAdvice() {
+            
+            var uri = 'https://api.adviceslip.com/advice'
+            var onSuccess = function(data){
+                var obj = JSON.parse(data);
+                document.getElementById("advice").innerHTML = obj.slip.advice;   
+            }
+            $.get(uri, onSuccess);
+
+        }
+        
+        function Advice(){
+			// These varibles are private
+			var covidStatsObject = {};
+			//Call to read in the address and turn this into a report
+			covidStatsObject.update = function (){
+				var country = get_name_value("country", "United Kingdom");
+				updateStats(country);
+			}
+
+            covidStatsObject.newAdvice = function(){
+                //if (timerId) clearInterval(timerId);
+                timerId = setInterval(myAdvice, 6e4); 
+                myAdvice();
+            }
+			//return the intialised object
+			return covidStatsObject;
+		}
+		//Create the Weather object, visible in the HTML file as app.weather
+		app.advice = new Advice();
+        app.advice.newAdvice();
+	}
+};
+app.initialize();
+
